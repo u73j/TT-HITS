cd8151dfd02f0ea4624eb6873a0588e5eb9395f4
/   :    DXRKN <abduh.exe@gmail.com>
2024-09-16 01:24:32 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,31 +3,14 @@ from threading import Thread
 
 # المتغيرات المطلوبة
 hit = 0
-gg = 0
-bb = 0
-go = 0
-bm = 0
 
 app = Flask('')
 
 @app.route('/')
 def home():
     return f'''
-    <pre>
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [0] Dev : @maho_s9 | @maho9s | TikTok Free Tool
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [1] HIT  -  تم الصيد    » 「{hit}」
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [2] Available Tik - متاح   » 「{gg}」
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [3] BAD Tik - مش متاح   » 「{bb}」
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [4] Good GM - جيميل صح » 「{go}」
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    [5] BAD - ايميل خاطئ   » 「{bm}」
-    ━━━━━━━━━━━━━━━━━━━━━━━━━
-    </pre>
+    <h1>Hit Count</h1>
+    <p>Number of Hits: {hit}</p>
     '''
 
 def run():
@@ -36,4 +19,3 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
-  
